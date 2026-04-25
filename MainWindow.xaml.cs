5e761e943f0841fbc842070609a96cb408915a49
Jhonatan Henryck de Souza Alves <jhonatan.alves@ondacom.com>
2026-04-25 12:45:22 -0300

    postit-sim_v1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace postitSimulator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DragArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void TextPost_changed(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var text = TextPost.Text;
+
+            var firstLine = text
+                .Split(new[] { "\r\n", "\n" }, StringSplitOptions.None)
+                .FirstOrDefault();
+
+            TitlePost.Text = string.IsNullOrWhiteSpace(firstLine)
+                ? "Post-it"
+                : firstLine;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
